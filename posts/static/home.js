171017b87d3a817149464f059f181e3c4c70864f
Celine Tran <celinephoungtran@gmail.com>
2023-08-29 17:24:53 -0400

    Add character count

--- posts/static/home.js
+++ posts/static/home.js
@@ -1,12 +1,21 @@
 $(document).ready(function () {
+
     $(".post-text").on('keydown paste', function (event) {
         var cntMaxLength = parseInt($(this).attr('maxlength'));
         
         const isValidShortcut = (event.ctrlKey && event.keyCode != 86 );
         const isValidKeyCode = [8, 16, 17, 37, 38, 39, 40, 46].includes(event.keyCode);
+
+        var length = $(this).text().length;
     
         if ($(this).text().length >= cntMaxLength && !isValidKeyCode && !isValidShortcut) {
             event.preventDefault();
+        } else if (!isValidKeyCode && !isValidShortcut) {
+            length++;
+        } else if ((event.keyCode == 8 || event.keyCode === 46) & length > 0) {
+            length--;
         }
+
+        $("#char-count").text(`${length} / ${cntMaxLength} characters`);
     });
 })
