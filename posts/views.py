57096b9d476a1e17bf8ce9b92e268233085969a9
Celine Tran <celinephoungtran@gmail.com>
2023-08-30 17:54:37 -0400

    add user name and order into timeline

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse
-from .models import Tweet
+from .models import Tweet, User
 
 # Create your views here.
 def home(request):
-    items = Tweet.objects.all()
+    items = Tweet.objects.all().order_by("-updated_at")
     return render(request, "home.html", {"Tweets": items})
