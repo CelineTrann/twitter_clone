7cd260ae42bd02c92a80bdddb2c5a629aeba5e33
Celine Tran <celinephoungtran@gmail.com>
2023-09-01 19:52:25 -0400

    Create path to user profile

--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
-from .models import Tweet, User
+from .models import Tweet, Profile
 from .forms import TweetForm
 
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from django.http import HttpResponse
+
 @login_required
 def home(request):
     items = Tweet.objects.all().order_by("-updated_at")
     form = TweetForm()
-    return render(request, "home.html", {"Tweets": items, "form": form})
+    return render(request, "home.html", {"Tweets": items, "form": form, "User": request.user})
 
+@login_required
 def post(request):
     if request.method == 'POST':
         form = TweetForm(request.POST)
@@ -19,4 +21,10 @@ def post(request):
             tweet.user = request.user
             tweet.save()
             return redirect("home")
+        
+@login_required
+def profile(request, username):
+    profile_info = Profile.objects.get(pk=request.user.id)
+    return render(request, "profile.html", {"Profile": profile_info})
+    
         
