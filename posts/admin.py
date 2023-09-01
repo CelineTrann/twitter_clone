59fb6921dd73d93a0794cee96e30507d4e5bb0df
Celine Tran <celinephoungtran@gmail.com>
2023-09-01 15:10:43 -0400

    join User and Profile model on admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import Tweet, Profile
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    inlines = [ProfileInline]
+
 # Register your models here.
-admin.site.register(Profile)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Tweet)
