5d52f2ba4fe271061594388d76728bf1434a071d
Celine Tran <celinephoungtran@gmail.com>
2023-09-01 19:16:34 -0400

    username is associated with tweet

--- posts/views.py
+++ posts/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Tweet, User
 from .forms import TweetForm
 
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def home(request):
     items = Tweet.objects.all().order_by("-updated_at")
     form = TweetForm()
@@ -14,6 +15,8 @@ def post(request):
     if request.method == 'POST':
         form = TweetForm(request.POST)
         if form.is_valid():
-            form.save()
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
             return redirect("home")
         
