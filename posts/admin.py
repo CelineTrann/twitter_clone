7d94a6c316b1c7bd5625bc49f63ca9ebb19135a0
Celine Tran <celinephoungtran@gmail.com>
2023-09-01 14:59:57 -0400

    Created associated user profile

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Tweet
+from .models import Tweet, Profile
 
 # Register your models here.
+admin.site.register(Profile)
 admin.site.register(Tweet)
--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=60)
+    bio = models.CharField(max_length=255)
+    follows = models.ManyToManyField(
+        "self",
+        related_name="followed_by",
+        symmetrical=False,
+        blank=True
+    )
+
 class Tweet(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, blank=True)
     content = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
