86176519ef139dd0b647054a311c29986442d164
Celine Tran <celinephoungtran@gmail.com>
2023-09-01 21:14:55 -0400

    make tweets more readable in admin

--- posts/models.py
+++ posts/models.py
@@ -31,3 +31,6 @@ class Tweet(models.Model):
     content = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
