cb720d2bd8ffe185fc1e4d6f70287bee13628709
Celine Tran <celinephoungtran@gmail.com>
2023-09-07 18:34:14 -0400

    create tweet component

---
+++ posts/static/components/create-tweet-form.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    // Post Character Count
+    $("#id_direct-content").keyup(function () {
+        $("#direct-char-count").text($(this).val().length + " / 255 characters");
+    });
+
+    $("#id_modal-content").keyup(function () {
+        $("#modal-char-count").text($(this).val().length + " / 255 characters");
+    });
+
+    // Auto resize text area
+    $('.tweet-content').on('input', function () {
+        this.style.height = 'auto'; 
+        this.style.height = (this.scrollHeight) + 'px'; 
+    });
+
+    // Disable & Enable Post Button
+    $('#direct-submit-btn').prop('disabled', true);
+    $('#modal-submit-btn').prop('disabled', true);
+    disable_submit("#direct-submit-btn", "#id_direct-content")
+    disable_submit("#modal-submit-btn", "#id_modal-content")
+
+    function disable_submit (btn_id, textarea_id) {
+        $(textarea_id).on('input', function () {
+            if ($(this).val().trim() !== '') {
+                $(btn_id).prop('disabled', false);
+            } else {
+                $(btn_id).prop('disabled', true);
+            }
+        })
+    }
+});
